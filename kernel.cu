a10291dd7871ec9f561cc0dc88e5e2422913bcc1
vitalitylearning2021 <83769781+vitalitylearning2021@users.noreply.github.com>
2021-12-24 08:10:42 +0100

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,119 @@
+//#include <iostream>
+//#include "vec3.h"
+//#include "ray.h"
+//
+//// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
+//#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+//
+//void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+//	if (result) {
+//		std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+//			file << ":" << line << " '" << func << "' \n";
+//		// Make sure we call CUDA Device Reset before exiting
+//		cudaDeviceReset();
+//		exit(99);
+//	}
+//}
+//
+//__device__ bool hit_sphere(const vec3& center, float radius, const ray& r) {
+//	vec3 oc = r.origin() - center;
+//	float a = dot(r.direction(), r.direction());
+//	float b = 2.0f * dot(oc, r.direction());
+//	float c = dot(oc, oc) - radius*radius;
+//	float discriminant = b*b - 4.0f*a*c;
+//	return (discriminant > 0.0f);
+//}
+//
+//__device__ vec3 color(const ray& r) {
+//	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+//		return vec3(1, 0, 0);
+//	vec3 unit_direction = unit_vector(r.direction());
+//	float t = 0.5f*(unit_direction.y() + 1.0f);
+//	return (1.0f - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+//}
+//
+//__global__ void render(vec3 *fb, int max_x, int max_y,
+//	vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
+//	int i = threadIdx.x + blockIdx.x * blockDim.x;
+//	int j = threadIdx.y + blockIdx.y * blockDim.y;
+//	if ((i >= max_x) || (j >= max_y)) return;
+//	int pixel_index = j*max_x + i;
+//	float u = float(i) / float(max_x);
+//	float v = float(j) / float(max_y);
+//	ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+//	fb[pixel_index] = color(r);
+//}
+//
+//int main() {
+//	int nx = 1200;
+//	int ny = 600;
+//	int tx = 8;
+//	int ty = 8;
+//
+//	std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+//	std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+//
+//	int num_pixels = nx*ny;
+//	size_t fb_size = num_pixels * sizeof(vec3);
+//
+//	// allocate FB
+//	vec3 *fb;
+//	checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+//
+//	clock_t start, stop;
+//	start = clock();
+//	// Render our buffer
+//	dim3 blocks(nx / tx + 1, ny / ty + 1);
+//	dim3 threads(tx, ty);
+//	render << <blocks, threads >> >(fb, nx, ny,
+//		vec3(-2.0, -1.0, -1.0),
+//		vec3(4.0, 0.0, 0.0),
+//		vec3(0.0, 2.0, 0.0),
+//		vec3(0.0, 0.0, 0.0));
+//	checkCudaErrors(cudaGetLastError());
+//	checkCudaErrors(cudaDeviceSynchronize());
+//	stop = clock();
+//	double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+//	std::cerr << "took " << timer_seconds << " seconds.\n";
+//
+//	// Output FB as Image
+//	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+//	for (int j = ny - 1; j >= 0; j--) {
+//		for (int i = 0; i < nx; i++) {
+//			size_t pixel_index = j*nx + i;
+//			int ir = int(255.99*fb[pixel_index].r());
+//			int ig = int(255.99*fb[pixel_index].g());
+//			int ib = int(255.99*fb[pixel_index].b());
+//			std::cout << ir << " " << ig << " " << ib << "\n";
+//		}
+//	}
+//
+//	checkCudaErrors(cudaFree(fb));
+//}
+//
+////int main() {
+////
+////	// Image
+////
+////	const int image_width = 256;
+////	const int image_height = 256;
+////
+////	// renderingKernel
+////
+////	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+////
+////	for (int j = image_height - 1; j >= 0; --j) {
+////		for (int i = 0; i < image_width; ++i) {
+////			auto r = double(i) / (image_width - 1);
+////			auto g = double(j) / (image_height - 1);
+////			auto b = 0.25;
+////
+////			int ir = static_cast<int>(255.999 * r);
+////			int ig = static_cast<int>(255.999 * g);
+////			int ib = static_cast<int>(255.999 * b);
+////
+////			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+////		}
+////	}
+////	std::cerr << "\nDone.\n";
+////}
